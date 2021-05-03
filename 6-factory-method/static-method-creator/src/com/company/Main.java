71cc2ff005e26a842bf474d9eb618dbd98790d64
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-03 17:22:09 +0530

    Add static method creator for factory method

---
+++ 6-factory-method/static-method-creator/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Get an object of Circle and call its draw method
+        Shape shape1 = ShapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        // Get an object of Rectangle and call its draw method
+        Shape shape2 = ShapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        // Get an object of Square and call its draw method
+        Shape shape3 = ShapeFactory.getShape("SQUARE");
+        shape3.draw();
+    }
+}
