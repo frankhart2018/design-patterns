2c1a8552bf6be9b48c9dd381e861707347c6a38a
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-04 19:43:58 +0530

    Add eager evaluation method for singleton creation

---
+++ 8-singleton/eager-evaluation/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+class Singleton {
+
+    // The private reeference to the one and only instance
+    // This will create the instance even if it isn't used
+    private static Singleton uniqueInstance = new Singleton();
+
+    // An instance attribute
+    private int data = 0;
+
+    /*
+     * The Singleton Constructor
+     * Note that it is private
+     * No client can instantiate a Singleton object!
+     */
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return uniqueInstance;
+    }
+
+    public void setData(int myData) {
+        data = myData;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+
+        // Set the data value
+        s.setData(55);
+
+        System.out.println("First reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+
+        // Get another reference to the Singleton
+        // Is it the same object?
+
+        s = null;
+        s = Singleton.getInstance();
+
+        System.out.println("\nSecond reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+    }
+}
