52d388f7447fc0e67de8200975515649a02cb40b
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-04 19:42:16 +0530

    Add double checked locking method for singleton creation

---
+++ 8-singleton/double-checked-locking/src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+class Singleton {
+
+    // The private reeference to the one and only instance
+    // Volatile allows multiple threads to change value of a variable
+    private volatile static Singleton uniqueInstance = null;
+
+    // An instance attribute
+    private int data = 0;
+
+    /*
+     * The Singleton Constructor
+     * Note that it is private
+     * No client can instantiate a Singleton object!
+     */
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if(uniqueInstance == null) {
+            synchronized (Singleton.class) { // We only synchronize the first time through
+                if(uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+
+        return uniqueInstance;
+    }
+
+    public void setData(int myData) {
+        data = myData;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+
+        // Set the data value
+        s.setData(55);
+
+        System.out.println("First reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+
+        // Get another reference to the Singleton
+        // Is it the same object?
+
+        s = null;
+        s = Singleton.getInstance();
+
+        System.out.println("\nSecond reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+    }
+}
