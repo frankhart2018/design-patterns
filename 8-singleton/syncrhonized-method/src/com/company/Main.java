3a6afe87a245fe540e81947481b8c47b367fa72e
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-04 19:34:49 +0530

    Add synchronized method for singleton creation

---
+++ 8-singleton/syncrhonized-method/src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+class Singleton {
+
+    // The private reeference to the one and only instance
+    private static Singleton uniqueInstance = null;
+
+    // An instance attribute
+    private int data = 0;
+
+    /*
+     * The Singleton Constructor
+     * Note that it is private
+     * No client can instantiate a Singleton object!
+     */
+
+    private Singleton() {}
+
+    // By adding the syncrhonized keyword to getInstance()
+    // we force every thread to wait its turn before it can
+    // enter the method
+    public static synchronized Singleton getInstance() {
+        if(uniqueInstance == null)
+            uniqueInstance = new Singleton();
+
+        return uniqueInstance;
+    }
+
+    public void setData(int myData) {
+        data = myData;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+
+        // Set the data value
+        s.setData(55);
+
+        System.out.println("First reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+
+        // Get another reference to the Singleton
+        // Is it the same object?
+
+        s = null;
+        s = Singleton.getInstance();
+
+        System.out.println("\nSecond reference: " + s);
+        System.out.println("Singleton data value is: " + s.getData());
+    }
+}
