69025e241cd3124be905071d3b6b17b9cc2134b9
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-05 15:58:56 +0530

    Add dependency inversion in cpp

--- cpp/3-principles-and-strategies-of-design/dependency-inversion/dependency-inversion/Main.cpp
+++ cpp/3-principles-and-strategies-of-design/dependency-inversion/dependency-inversion/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-class MySqlConnection {
+class DBConnectionInterface {
+public:
+	virtual int connect() = 0;
+};
+
+class MySqlConnection : public DBConnectionInterface {
 public:
 	int connect() {
 		return 1;
@@ -9,11 +14,14 @@ public:
 
 class PasswordReminder {
 private:
-	MySqlConnection dbConnection;
+	DBConnectionInterface* dbConnection;
+
+public:
+	PasswordReminder(DBConnectionInterface* dbConnection) {
+		this->dbConnection = dbConnection;
+	}
 };
 
 int main() {
-
-
 	return 0;
 }
