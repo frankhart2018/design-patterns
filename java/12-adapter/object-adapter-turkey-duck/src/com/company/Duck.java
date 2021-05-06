df8ca2aeabee9bbe61ba26bbb87ebdf257845ee2
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-06 20:53:03 +0530

    Add first example of adapter - turkey/duck

---
+++ java/12-adapter/object-adapter-turkey-duck/src/com/company/Duck.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public interface Duck {
+    public void quack();
+    public void fly();
+}
+
+class MallardDuck implements Duck {
+
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I am flying");
+    }
+}
