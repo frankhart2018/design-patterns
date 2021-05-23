28884f5774c115ee76d782ec3a110081fa9ec332
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-05-23 20:33:34 +0530

    Add implementation for visitor pattern

---
+++ java/31-visitor/implementation/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    private static int calculatePrice(ItemElement[] items) {
+        ShoppingCartVisitor visitor = new ShoppingCartVisitorImpl();
+        int sum = 0;
+
+        for(ItemElement item: items) {
+            sum += item.accept(visitor);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+	    ItemElement[] items = new ItemElement[]{
+	            new Book(20, "1234"),
+                new Book(100, "5678"),
+                new Fruit(10, 2, "Banana"),
+                new Fruit(5, 5, "Apple")
+	    };
+
+	    int total = calculatePrice(items);
+        System.out.println("Total cost = " + total);
+    }
+}
