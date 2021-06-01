1f3d8e709deacfce58fadeab0bc9ef13b6f900f8
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-06-01 19:07:36 +0530

    Add java.io example for decorator

---
+++ head-first-design-patterns/3-the-decorator-pattern/2-java-io-decorators/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int c;
+        try {
+            InputStream in = new LoweCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+
+            while((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
